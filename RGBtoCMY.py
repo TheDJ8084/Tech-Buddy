d19ffd111e67a40597004659ba7efc22cc03dd04
TheDJ8084 <126491711+TheDJ8084@users.noreply.github.com>
2025-03-31 11:09:10 +0200

    V0.0.2-alpha

--- RGBtoCMY.py
+++ RGBtoCMY.py
@@ -1,10 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter.tix import COLUMN
 
 
 def calculate(*args):
-    pass
+    R = Red.get()
+    G = Green.get()
+    B = Blue.get()
 
 root = Tk()
 root.title("RGB to CMY")
@@ -32,7 +33,25 @@ Blue_label.grid(column=1, row=3)
 Blue_entry = ttk.Entry(mainframe, width=7, textvariable=Blue)
 Blue_entry.grid(column=2, row=3)
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3)
+Cyan = StringVar()
+Cyan_label = ttk.Label(mainframe, width=7, text="Cyan")
+Cyan_label.grid(column=4, row=1)
+Cyan_entry = ttk.Entry(mainframe, width=7, textvariable=Cyan)
+Cyan_entry.grid(column=5, row=1)
+
+Magenta = StringVar()
+Magenta_label = ttk.Label(mainframe, width=7, text="Magenta")
+Magenta_label.grid(column=4, row=2)
+Magenta_entry = ttk.Entry(mainframe, width=7, textvariable=Magenta)
+Magenta_entry.grid(column=5, row=2)
+
+Yellow = StringVar()
+Yellow_label = ttk.Label(mainframe, width=7, text="Yellow")
+Yellow_label.grid(column=4, row=3)
+Yellow_entry = ttk.Entry(mainframe, width=7, textvariable=Yellow)
+Yellow_entry.grid(column=5, row=3)
+
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=4)
 
 for child in mainframe.winfo_children():
     child.grid_configure(padx=5, pady=5)
